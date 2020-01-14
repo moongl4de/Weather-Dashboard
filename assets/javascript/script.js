6594f178df408a0014bb0a933827bd26922ebc28
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-01-14 17:45:26 -0500

    hopefully mitigating a catastrophe lmao

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,84 @@
 var APIKey = "0234a5db053f0c8c1789c2487e8d0de2";
-var city = "Atlanta"
-    
+var buttonDiv = $(".button-div")
+var cityButtonArr = []
+var testCounter = 0
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  // Create CODE HERE to Log the queryURL
-  console.log(queryURL)
-  // Create CODE HERE to log the resulting object
-  console.log(response)
-  // Create CODE HERE to transfer content to HTML
-  $(".city").append(response.name)
-  $(".city").attr("style", "font-weight: bold; font-size: 30px", )
-  $(".wind").append("Wind speed " + response.wind.speed)
-  $(".humidity").append("Humidity: " + response.main.humidity)
-  var convert = response.main.temp
-  var F = (convert - 273.15) * 1.80 + 32
-  F = F.toFixed(2)
-  console.log(F)
-  $(".temp").append("Temperature: " + F)
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-
-  // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-  // Create CODE HERE to dump the temperature content into HTML
+$("#form").on('submit', function(e){
+  e.preventDefault();
+  var city=""
   
+  city = $("#city-input").val()
+  
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-});
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+   
+  }).then(function(response) {
+    console.log(queryURL)
+    
+    console.log(response)
+    $("#city-title").empty()
+    $("#city-title").append(response.name)
+    $(".city").attr("style", "font-weight: bold; font-size: 30px", )
+    $("#wind-speed").empty()
+    $("#wind-speed").append("Wind speed: " + response.wind.speed + " MPH")
+    var convert = response.main.temp
+    var F = (convert - 273.15) * 1.80 + 32
+    F = F.toFixed(0)
+    console.log(F)
+    $("#temperature").empty()
+    $("#temperature").append("Temperature: " + F + "°")
+    
+    generateButton();
+
+    function generateButton() {
+      $("#city-input").text("")
+      var create = $("<button>")
+      create.attr("class", "btn btn-outline-secondary")
+      create.attr("type", "button")
+      create.text(response.name)
+      buttonDiv.prepend(create)
+      
+      var cityString = response.name
+      cityButtonArr.push(cityString.toString())
+      
+      localStorage.setItem("cityStorage", JSON.stringify(cityButtonArr))
+      console.log("cityarray" + cityButtonArr)
+      
+    }
+  }
+  )
+})
+
+loadData();
+
+function loadData(){
+  console.log("array" + cityButtonArr)
+  var loadData = localStorage.getItem("cityStorage")
+  var loadedArr = JSON.parse(loadData)
+  console.log("loadedArr" + loadedArr) 
+
+ 
+
+  if(loadedArr != null && loadedArr != ""){
+  cityButtonArr.push(loadedArr)
+  for(i=0; i<cityButtonArr.length;i++){
+    console.log("i" + i)
+    var create = $("<button>")
+    create.attr("class", "btn btn-outline-secondary")
+    create.attr("type", "button")
+    create.text(cityButtonArr[i])
+    buttonDiv.prepend(create)
+  }
+}
+ 
+  console.log(cityButtonArr.length)
+}
+
+
+
+
+
